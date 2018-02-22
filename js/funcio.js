725fe805dee8a8bc8dff573bcc6a6d9cfd300dce
Your Name <you@example.com>
2018-02-22 13:50:13 +0100

    ajax

--- js/funcio.js
+++ js/funcio.js
@@ -1,5 +1,40 @@
 
+function convert(){
+    var tag = $('#tag').val();
+    var etiq = tag.replace(/[ ]/gi,',');
+    return etiq.toString();
+    /*cridada ajax
+    succes executa una funcio on a dedins hi haura Data.items i una fucnio. per cada item me fas una fila i depres hu afegim a una taula.
+    si hi ha un error mos fara una altra funcio on sortira un alert...*/
+}
 function cercar(){
-    tag = $('#tag').val().split(" ");
-    console.log(tag);
+    
+    //$('filaresultat').remove();
+    var tag = convert();
+    onclick = $.ajax({
+        url: "https://api.flickr.com/services/feeds/photos_public.gne",
+        type: 'Get',
+        async: 'true',
+        dataType: 'json',
+        data: 'tags='+ $('#tag').val()+'&tagmode='+$('#tipus').val()+'format=json&jsoncallback=?',
+        success: mostrar,
+        error: function(jqXHR, exception){
+            alert(JSON.parse(jqXHR.responseText));
+        }
+    });
+    return false;
+}
+function mostrar(data){
+    $.each(data.items, function(i, item){
+        var fila = $("<tr class='filaresultat'><td>"+
+                    '<h4>'+item.title+'</h4></td>'+
+                    '<td><a href ="'+item.link+'"></a></td>'+
+                    '<td><img src='+item.media.m+'></td>'+
+                    '<td><p'+item.published.substr(-10)+'</p></td>'+
+                    '<td><p'+item.author+'</p></td>'+
+                    '<td><i'+item.tags+'></i></td>'+
+                    "</tr>");
+        
+        $('#esquema').append(fila);
+    })
 }
